08d320d7447925332e0e48713139c12e948fefc8
Adam Davis <davisadam10@googlemail.com>
2015-01-11 12:45:10 +0000

    Added new urls

--- southernWeb/urls.py
+++ southernWeb/urls.py
@@ -20,6 +20,8 @@ urlpatterns = patterns(
     url(r'^logout/$', 'delayRepay.views.logout', name='logout'),
     url(r'^addJourney/$', 'delayRepay.views.addJourney', name='addJourney'),
     url(r'^addTicket/$', 'delayRepay.views.addTicket', name='addTicket'),
+    url(r'^noTicket/$', 'delayRepay.views.noTicket', name='noTicket'),
+    url(r'^delaySuccess/$', 'delayRepay.views.delaySuccess', name='delaySuccess'),
     url(r'^register/$', 'delayRepay.views.register_user', name='invalid_login'),
     url(r'^register_success/$', 'delayRepay.views.register_success', name='invalid_login'),
 
