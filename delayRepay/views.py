01e6dcd7b07cffe5ab3786fc32806a7f7fb3f583
Adam Davis <davisadam10@googlemail.com>
2015-01-11 14:23:54 +0000

    forcing html response

--- delayRepay/views.py
+++ delayRepay/views.py
@@ -96,6 +96,7 @@ def index(request):
                     delay.delete()
                     return HttpResponseRedirect('/noTicket')
 
+                return HttpResponse(success)
                 return HttpResponseRedirect('/delaySuccess')
             else:
                 args['form'] = form
