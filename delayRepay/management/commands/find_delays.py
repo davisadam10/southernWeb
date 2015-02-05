98cc9d1de33ecc0d2d0a39901ec89ae10b4fe1e2
Adam Davis <davisadam10@googlemail.com>
2015-02-05 12:29:52 +0000

    Updated the find_delays to look working for arrivals which have been cancelled, need to work on departures

--- delayRepay/management/commands/find_delays.py
+++ delayRepay/management/commands/find_delays.py
@@ -1,6 +1,6 @@
 __author__ = 'adam'
 
-from datetime import datetime
+from datetime import datetime, time
 from django.core.management.base import BaseCommand
 import delayRepay.models as models
 import delayRepay.utils as utils
@@ -12,12 +12,11 @@ class Command(BaseCommand):
 
         users = models.UserData.objects.all()
         for user in users:
-            delay_found = None
-            already_claimed_today = None
+            delay_found = False
+            already_claimed_today = utils.get_delays_for_today(user)
             journeys = utils.get_user_journeys(user)
-            count = 0
             for journey in journeys:
-                if not delay_found:
+                if not delay_found and not already_claimed_today:
                     arriving_station = utils.get_station_from_name(journey.arrivingStation)
                     departing_station = utils.get_station_from_name(journey.departingStation)
 
@@ -25,11 +24,24 @@ class Command(BaseCommand):
                         departing_station.short_name,
                         arriving_station.short_name
                     )
-                    for service in services:
+
+                    services2 = nationalRailUtils.get_services_arriving_from(
+                        departing_station.short_name,
+                        arriving_station.short_name
+                    )
+
+                    all_services = services + services2
+                    for service in all_services:
                         cancelled = nationalRailUtils.is_service_cancelled(service)
                         # Should be cancelled but for debugging I have more info
                         if cancelled:
-                            hour, minute = service.std.split(':')
+                            arrival = False
+                            if 'sta' in service:
+                                hour, minute = service.sta.split(':')
+                                arrival = True
+                            else:
+                                hour, minute = service.std.split(':')
+
                             service_origin = service.origin.location[0].locationName
                             if journey.departingStation != service_origin:
                                 print "%s Found A Cancelled Train To %s calling at %s starting from %s @ %s:%s" % (
@@ -42,23 +54,46 @@ class Command(BaseCommand):
                                 )
                             else:
                                 # Easy As departing station is end of the line so no problems
-                                print "%s Found A Cancelled Train To %s calling at %s starting from %s @ %s:%s" % (
+                                extra = ''
+                                if arrival:
+                                    extra = ' Arriving '
+
+                                print "%s Found A Cancelled Train To %s calling at %s starting from %s %s @ %s:%s" % (
                                     user.forename,
                                     journey.arrivingStation,
                                     journey.departingStation,
                                     service_origin,
+                                    extra,
                                     hour,
                                     minute
                                 )
-                                #print service
-                                #nationalRailUtils.findServiceArrival(arriving_station.short_name, service.serviceID)
+                                if arrival:
+                                    departure_details = nationalRailUtils.getServiceDepartDetails(service.serviceID)
+                                    newDelay = models.Delay()
+                                    newDelay.startTime = departure_details['startTime']
+                                    newDelay.endTime = time(int(hour), int(minute))
+                                    newDelay.delay = '30-59 mins'
+                                    newDelay.delay_reason = 'Train cancelled'
+                                    newDelay.delayRepayUser = user
+                                    newDelay.journey = journey
+                                    newDelay.date = datetime.now().date()
+                                    newDelay.save()
+                                    delay_found = True
+                                    friends = user.friends.all()
+                                    for friend in friends:
+                                        delays_on_date = utils.get_delays_for_date(friend, newDelay.date)
+                                        if not delays_on_date:
+                                            newDelay.claimed = False
+                                            newDelay.pk = None
+                                            newDelay.delayRepayUser = friend
+                                            newDelay.save()
 
-                                '''
-                                newDelay = models.Delay()
-                                newDelay.delayRepayUser(user)
-                                newDelay.journey(journey)
-                                newDelay.date = datetime.now().date()
-                                '''
+
+
+
+
+
+
 
 
 
