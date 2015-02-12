70129ff0e436a341c3e27228ee3553a388125f8b
Adam Davis <davisadam10@googlemail.com>
2015-02-12 21:17:44 +0000

    Added email tester

---
+++ delayRepay/management/commands/test_email.py
@@ -0,0 +1,16 @@
+__author__ = 'adam'
+
+
+from django.core.management.base import BaseCommand
+import delayRepay.models as models
+from django.core.mail import send_mail
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = models.UserData.objects.filter(username='davisadam10')[0]
+        send_mail(
+            'Test', 'Testing the email',
+            'admin@southern-fail.co.uk',
+            [str(user.email)]
+        )
