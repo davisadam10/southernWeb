9c97a8c76959d3b0a4e2c729dfa699066cc0c81c
Adam Davis <davisadam10@googlemail.com>
2015-02-12 21:46:21 +0000

    Reverting to django backend

--- delayRepay/management/commands/test_email.py
+++ delayRepay/management/commands/test_email.py
@@ -1,22 +1,17 @@
 __author__ = 'adam'
 
 
-import os
+
 from django.core.management.base import BaseCommand
 import delayRepay.models as models
-from postmark import PMMail
+from django.core.mail import send_mail
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = models.UserData.objects.filter(username='davisadam10')[0]
-        message = PMMail(
-            api_key=os.environ.get('POSTMARK_API_TOKEN'),
-            subject="Test",
-            sender="admin@southern-fail.co.uk",
-            to=str(user.email),
-            text_body="Hello",
-            tag="testEmail"
+        send_mail(
+            'New Delay', 'Hi',
+            'admin@southern-fail.co.uk',
+            [str(user.email)]
         )
-
-        message.send()
