be791ad1fec1b3a8d4f18f23d6edb7e72ef17dad
Adam Davis <davisadam10@googlemail.com>
2015-03-01 15:41:20 +0000

    adding database name for testing jenkins

--- southernWeb/settings.py
+++ southernWeb/settings.py
@@ -71,6 +71,7 @@ DATABASES = {
 
 DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
+DATABASES['default']['NAME'] = 'southernweb'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
