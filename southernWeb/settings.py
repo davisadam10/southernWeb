277234a80ea58189784e94ef063bf589783228b8
Adam Davis <davisadam10@googlemail.com>
2015-03-01 15:15:58 +0000

    Added django jenkins to requirements and reports

--- southernWeb/settings.py
+++ southernWeb/settings.py
@@ -114,5 +114,10 @@ REST_FRAMEWORK = {
 JENKINS_TASKS = (
     'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_csslint',
+    'django_jenkins.tasks.run_sloccount'
 )
 
