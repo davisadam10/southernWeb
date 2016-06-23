a6a2375d192a46166ceb4c9fdd4365b41c291e33
davisadam10 <davisadam10@googlemail.com>
2016-06-23 21:42:23 +0100

    Improve the deleting of the delays

--- delayRepay/management/commands/expire_delays.py
+++ delayRepay/management/commands/expire_delays.py
@@ -2,15 +2,19 @@ __author__ = 'adam'
 
 from django.core.management.base import BaseCommand
 import delayRepay.models as models
-from datetime import datetime
+from datetime import datetime, timedelta
+
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         delays = models.Delay.objects.filter(expired=False)
         todays_date = datetime.now().date()
+        delayCutoff = datetime.now() + timedelta(-28)
         for delay in delays:
-            if (todays_date - delay.date).days > 28:
+            print delay.date, type(delay.date)
+            print delayCutoff, type(delayCutoff)
+            if delay.date < delayCutoff:
                 delay.expired = True
                 delay.delete()
                 print 'Expired'
